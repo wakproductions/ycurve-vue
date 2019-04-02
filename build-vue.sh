888f469da133d6ba7801861444d204ed68c40725
Winston Kotzan <wak@wakproductions.com>
2019-04-01 20:19:06 -0400

    Improve front end build script

--- build-vue.sh
+++ build-vue.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 cd ./ycurve-frontend
 yarn run build
-mv -rf dist/* ../ycurve-backend/public
+rm -r ../ycurve-backend/public/js
+rm -r ../ycurve-backend/public/css
+rm -r ../ycurve-backend/public/index.html
+mv dist/* ../ycurve-backend/public
 cd ../
