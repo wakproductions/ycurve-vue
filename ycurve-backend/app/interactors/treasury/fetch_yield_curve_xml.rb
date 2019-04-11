ace95f9b59a8b6813f5fdcea5d7c06b9b6d30074
Winston Kotzan <wak@wakproductions.com>
2019-04-11 19:00:36 -0400

    Moved some backend files from lib to interactors directory

--- ycurve-backend/app/interactors/treasury/fetch_yield_curve_xml.rb
+++ ycurve-backend/app/interactors/treasury/fetch_yield_curve_xml.rb
@@ -12,6 +12,8 @@ module Treasury
 
     private
 
+    # Web page:
+    # https://www.treasury.gov/resource-center/data-chart-center/interest-rates/pages/textview.aspx?data=yield
     def api_uri
       @api_url ||= URI.parse(
         URI::encode("http://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE) eq #{month} and year(NEW_DATE) eq #{year}")
